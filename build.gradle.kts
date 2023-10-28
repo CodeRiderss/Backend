7b0ed2af75da5a210af96d626f78734a0836918e
stefhol <hoef.stef@pm.me>
2023-10-28 15:04:38 +0200

    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("org.postgresql:postgresql")
+    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly ("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
