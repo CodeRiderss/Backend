3b9f4a8770c1299f60d7608918b62b2457a99e1c
chuuda <chuuda@chuuda.de>
2023-10-28 18:56:41 +0200

    Add router

--- src/main/kotlin/com/coderiders/coderiders/controllers/RatingController.kt
+++ src/main/kotlin/com/coderiders/coderiders/controllers/RatingController.kt
@@ -4,29 +4,76 @@ import com.coderiders.coderiders.model.Rating
 import com.coderiders.coderiders.repository.CarRepository
 import com.coderiders.coderiders.repository.RatingRepository
 import com.coderiders.coderiders.repository.UserRepository
+import jakarta.persistence.*
 import org.springframework.web.bind.annotation.*
 import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 @RestController
 class RatingController(
-        val carRepository: CarRepository,
+        val RatingRepository: RatingRepository,
         private val userRepository: UserRepository,
         private val ratingRepository: RatingRepository
 ) {
     @GetMapping("/rating")
-    fun getCars(): List<Rating> {
+    fun getRating(): List<Rating> {
         return ratingRepository.findAll()
     }
-    @DeleteMapping("/{userId}/rating/{carId}")
-    fun deleteCar(userId: Long, ratingId: Long) {
+
+    class RatingRequest(
+            val rating: Int,
+            val description: String
+
+    )
+
+    @PostMapping("/{userId}/rating")
+    fun insertRating(userId: Long, @RequestBody rating: RatingRequest): Rating {
+        val user = userRepository.findById(userId)
+        user.getOrNull()?.let {
+            return ratingRepository.save(
+                    Rating(
+                            rating = rating.rating,
+                            description = rating.description
+
+                    )
+            )
+        }
+        throw EntityNotFoundException()
+
+
+    }
+
+
+    @DeleteMapping("/{userId}/rating/{ratingId}")
+    fun deleteRating(userId: Long, ratingId: Long) {
         ratingRepository.deleteById(ratingId)
 
     }
 
-    data class UpdateCarRequest(
-            val model: Optional<String>,
-            val buildYear: Optional<Int>
+    data class UpdateRatingRequest(
+            val rating: Optional<Int>,
+            val description: Optional<String>,
 
-    )
+            )
 
+    @PatchMapping("/{userId}/rating/{ratingId}")
+    fun updateRating(userId: Long, ratingId: Long, updateRatingRequest: UpdateRatingRequest): Rating {
+        val user = userRepository.findById(userId)
+        user.getOrNull()?.let {
+            val rating = ratingRepository.findById(ratingId)
+            rating.getOrNull()?.let { rating ->
+                val newRating = Rating(
+                        id = rating.id,
+                        rating = updateRatingRequest.rating.orElse(rating.rating),
+                        description = updateRatingRequest.description.orElse(rating.description),
+
+                        )
+                return ratingRepository.save(newRating)
+
+
+            }
+
+        }
+        throw EntityNotFoundException()
+    }
 }
